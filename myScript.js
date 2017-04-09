b9d415a59b6e0b4fa5dd153e6c79a14c1fae98a3
MillerRobert17 <ram14@live.missouristate.edu>
2017-04-09 04:59:01 -0500

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -4,13 +4,12 @@ function validateForm() {
     var x = document.forms["ContactForm"]["firstname"].value;
     var y = document.forms["ContactForm"]["lastname"].value;
     var z = document.forms["ContactForm"]["emailaddress"].value;
+	if (x==null || x=="" || y==null || y=="" || z==null || z=="") {alert("Please Fill out all the the fields");return false;}}
 
-    	if (x==null || x=="" || y==null || y=="" || z==null || z=="") {
-    		alert("Please Fill out all the the fields");
-    		return false;
-    	}
-    }
-
- // date 
+// date 
 var d = new Date();
 document.getElementById("date").innerHTML = d;
+
+// dropdown
+function myFunction() {document.getElementById("dropdown1").classList.toggle("show");}
+window.onclick = function(event) {if (!event.target.matches('.dropdownbutton')) {var dropdowns = document.getElementsByClassName("dropdownpages");var i;for (i = 0; i < dropdowns.length; i++) {var openDropdown = dropdowns[i];if (openDropdown.classList.contains('show')) {openDropdown.classList.remove('show');}}}}
